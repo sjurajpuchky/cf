b4e58c2a0c80ed7378a6c3694fcef8a58339aba9
sjurajpuchky <sjurajpuchky@seznam.cz>
2015-10-22 17:24:41 +0200

    Added mailer class

--- Mailer.php
+++ Mailer.php
@@ -1,10 +1,22 @@
 <?php
-
+namespace cf;
 /**
  * Description of Mailer
  *
  * @author Puchky Juraj
  */
 class Mailer {
-    //put your code here
+    private $to;
+    private $subject;
+    private $message;
+    private $additional_headers = "";
+    function __construct($from,$to,$subject,$message) {
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->message = $message;
+        $this->additional_headers .= "From: $from\r\n";
+    }
+    public function send() {
+       return mail($this->to, $this->subject, $this->message, $this->additional_headers);
+    }
 }
